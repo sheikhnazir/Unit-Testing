fb974ce666fe37480fde2d6417332ce62d637eb6
SheikhNazir <73103801+naazie@users.noreply.github.com>
2023-09-05 10:45:31 +0530

    adding tearing up and tear down testing methods

--- src/test/java/com/example/UnitTesting/repo/PersonRepoTest.java
+++ src/test/java/com/example/UnitTesting/repo/PersonRepoTest.java
@@ -2,6 +2,8 @@ package com.example.UnitTesting.repo;
 
 import com.example.UnitTesting.Models.Person;
 import com.example.UnitTesting.Repository.PersonRepo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,5 +27,20 @@ class PersonRepoTest {
         Boolean actual = personRepo.isPersonExitsById(12);
 
         assertThat(actual).isTrue();
+
+//        assertThat(actual).isFalse();
+    }
+
+
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("This method is getting executed after each method testing...");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("This method is getting executed before each method testing...");
+
     }
 }
